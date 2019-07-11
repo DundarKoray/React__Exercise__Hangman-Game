32fa468cd569eb0ed24eaaea5673203bc97a53f6
Koray Dundar <koraydundar_40@hotmail.com>
2019-07-11 18:27:10 +0300

    added alt for images.

--- src/Hangman.js
+++ src/Hangman.js
@@ -65,10 +65,11 @@ class Hangman extends Component {
 
   /** render: render game */
   render() {
+    const altText = `${this.state.nWrong}/${this.props.maxWrong} guesses`
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
-        <img src={this.props.images[this.state.nWrong]} />
+        <img src={this.props.images[this.state.nWrong]} alt={altText} />
         <p>Guessed Wrong: {this.state.nWrong}</p>
         <p className='Hangman-word'>
           {this.state.nWrong < this.props.maxWrong ? this.guessedWord() : this.state.answer}
